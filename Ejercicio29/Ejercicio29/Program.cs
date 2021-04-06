03a9f881f176937a876389171440600ee532107f
diego.taub <diego.taub@accenture.com>
2021-04-06 20:43:07 -0300

    Ejercicio29

---
+++ Ejercicio29/Ejercicio29/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una fecha: ");
+            DateTime Fecha;
+            while (true)
+            {
+                if (DateTime.TryParse(Console.ReadLine(), out Fecha))
+                {
+                    break;
+                }
+
+                Console.Write("Fecha inválida. Ingrésela nuevamente: ");
+            }
+
+            DateTime FechaMesAnterior = new DateTime(Fecha.Year, Fecha.Month, 1).AddMonths(-1);
+
+            Console.Write($"El primer día del mes anterior de la fecha ingresada es: {FechaMesAnterior}");
+
+            Console.ReadKey();
+        }
+    }
+}
